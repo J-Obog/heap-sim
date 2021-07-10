7d0a38222960497b614900a101f66c1c7a821651
Joshua Obogbaimhe <jobogbaimhe@gmail.com>
2021-07-10 09:56:56 -0400

    converted executable sources to library

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-#include "heap.hpp" 
+#include "heap.hpp"
 
 int main() {
     HeapMem heap(512); 
     int ptr = heap.alloc(2); 
 
     heap.memset(ptr, 25644); 
-    heap.dump(); 
+    //heap.dump(); 
     
     return 0; 
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,11 +1,18 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "heap.hpp"
 
 bool isOdd(int x) {
     return !(x % 2 == 0); 
 }
 
+//should pass
 TEST_CASE("Sample test case to check if number is odd") {
     int num = 5; 
     REQUIRE(isOdd(num) == true); 
+}
+
+//should fail
+TEST_CASE("Another Test case") {
+    REQUIRE(1 == 3);
 }
