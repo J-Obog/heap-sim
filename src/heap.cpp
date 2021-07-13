fcb9aeb965af44626924a6fef30541ed7aefd444
Joshua Obogbaimhe <jobogbaimhe@gmail.com>
2021-07-13 14:14:15 -0400

    added appropriate commenting

--- src/heap.cpp
+++ src/heap.cpp
@@ -3,9 +3,11 @@
 #include <iomanip>
 
 HeapMem::HeapMem() {
-    const int default_size = 1024; 
+    const int default_size = 1024; //set default memory size to 1 KB
     _mem = new Word[default_size];
     _size = default_size; 
+
+    //zero out memory
     for(int i = 0; i < _size; i++) 
         _mem[i] = {0};
 }
@@ -13,6 +15,8 @@ HeapMem::HeapMem() {
 HeapMem::HeapMem(int size) {
     _mem = new Word[size]; 
     _size = size;
+
+    //zero out memory
     for(int i = 0; i < _size; i++) 
         _mem[i] = {0};  
 }
@@ -33,51 +37,53 @@ int HeapMem::alloc(int size) {
     int run = 0;
 
     for(int i = 0; i < _size; i++) {
-        if(run == size) {
+        if(run == size) { //found a contiguous block of memory big enough to allocate
             int ptr = i - size; 
             _mem_reserve(ptr, size);
             return ptr;
         }
-        run = (!_mem[i].alloc) ? run + 1 : 0; 
+        run = (!_mem[i].alloc) ? run + 1 : 0; //increment run if space is availabe, otherwise reset it
     }
 
     return -1;
 }
 
 void HeapMem::dealloc(int ptr) {
-    if(!_mem[ptr].head)
+    if(!_mem[ptr].head) //check if pointer is valid
         return; 
 
     int tmp = ptr; 
 
     do {
-        _mem[tmp] = {0}; 
+        _mem[tmp] = {0}; //clear block data
         tmp++; 
-    } while(_mem[tmp - 1].next != 0); 
+    } while(_mem[tmp - 1].next != 0); //halt if reached the end of the previously allocated block
 }
 
-int HeapMem::memget(int ptr) {
+int HeapMem::memget(int ptr) { //will implement soon
     return 0; 
 }
 
 void HeapMem::memset(int ptr, int val) {
-    if(!_mem[ptr].head)
+    if(!_mem[ptr].head) //check if pointer is valid
         return; 
 
-    int tmp = ptr, radix = 1; 
+    int tmp = ptr, radix = 1; //radix points to current base of val
 
     do {
-        uint8_t nth_byte = (uint8_t)(((1 << 8) - 1) & (val >> (radix - 1)));
+        uint8_t nth_byte = (uint8_t)(((1 << 8) - 1) & (val >> (radix - 1))); //extract a nth byte from val and store in current word
         _mem[tmp].data = nth_byte; 
         tmp++; 
-        radix += 8;  
-    } while(_mem[tmp - 1].next != 0);
+        radix += 8; //get the start position of the next byte
+    } while(_mem[tmp - 1].next != 0); //halt if reached the end of the previously allocated block 
 }
 
 void HeapMem::dump() {
-    for(int i = 0; i < _size; i += 16) {    
+    for(int i = 0; i < _size; i += 16) {
+        //print offset    
         std::cout << std::setfill('0') << std::setw(4) << std::hex << std::uppercase << i << "  ";
         for(int j = 0; j < 16; j++) {
+            //print memory
             std::cout << ' ' << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << (int)_mem[i+j].data;
         }
         std::cout << '\n'; 
--- src/heap.hpp
+++ src/heap.hpp
@@ -2,25 +2,27 @@
 #include <cstdint>
 
 struct Word {
-    uint8_t head : 1; 
-    uint8_t alloc : 1; 
-    uint8_t next : 1;  
-    uint8_t data : 8;
+    //flags
+    uint8_t head : 1; //indicates whether or not the word is the start of the allocated block
+    uint8_t alloc : 1; //indicates whether or not the word has been allocated 
+    uint8_t next : 1; //indicates whether or not the adjecent word is apart of the same allocated block
+    //payload
+    uint8_t data : 8; //bytes allocated for user data
 };
 
 class HeapMem {
     private:
-        Word* _mem;
-        int _size;
-        void _mem_reserve(int offset, int size); 
+        Word* _mem; //memory array
+        int _size; //size of memory
+        void _mem_reserve(int offset, int size); //helper function that sets the approriate flags
 
     public:
         HeapMem(); 
-        HeapMem(int size);
+        HeapMem(int size); 
         ~HeapMem(); 
         int alloc(int size);  
         int memget(int ptr); 
         void dealloc(int ptr);
-        void memset(int ptr, int val);  
-        void dump(); 
+        void memset(int ptr, int val); 
+        void dump();
 }; 
